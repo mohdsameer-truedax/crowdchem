6f3af7df916ee4e58daba56a6268b7bd866c0068
mohdsameer-truedax <mohdsameer@truedax.io>
2025-11-20 15:07:14 +0530

    Contactpagemobileui (#29)
    * search mobile responsive
    
    * usecase bug resolve 19/11
    
    * contact page mobile ui done

--- src/pages/Contact/HeadQuarter.tsx
+++ src/pages/Contact/HeadQuarter.tsx
@@ -5,13 +5,13 @@ const HeadQuarter = () => {
   const { t } = useTranslation();
   return (
     <div >
-      <section className="text-gray-600 body-font px-4 md:px-8 lg:px-12 xl92r:px-16 pb-6 md:pb-8 lg:pb-[2.625rem] 2xl:pb-[2.625rem] bg-background1 font-deca">
+      <section className="text-gray-600 body-font pb-20 sm:pb-0 px-4 md:px-8 lg:px-12 xl92r:px-16 pb-6 md:pb-8 lg:pb-[2.625rem] 2xl:pb-[2.625rem] bg-background1 font-deca">
         <div className="mx-auto  lg:text-[2.375rem] lg:leading-[3.75rem]  tracking-[0.08em] ">
           <div className="text-white" id='headquarters'>
-            <h1 className="title-font font-medium text-xl md:text-xl md:text-[1.2500rem] lg:text-[1.4250rem] xl92r:text-[1.5625rem] 2xl:text-[1.875rem] lg:leading-[6.25rem]  tracking-[0.04em]  font-nunito">
+            <h1 className="title-font font-medium text-[0.7rem] md:text-[1.2500rem] lg:text-[1.4250rem] xl92r:text-[1.5625rem] 2xl:text-[1.875rem] lg:leading-[6.25rem]  tracking-[0.04em]  font-nunito">
               {t('contact.ourheadquarter')}
             </h1>
-            <div className="h-[52.5rem] max-w-[52.5rem] lg:max-w-[59rem] xl92r:max-w-[65.62rem] 2xl:max-w-[78.75rem] lg:h-[59rem] xl92r:h-[65.62rem] 2xl:h-[78.75rem] rounded-lg overflow-hidden grayscale">
+            <div className="h-[89.74vw] max-w-[89.74vw] sm:h-[52.5rem] sm:max-w-[52.5rem] lg:max-w-[59rem] xl92r:max-w-[65.62rem] 2xl:max-w-[78.75rem] lg:h-[59rem] xl92r:h-[65.62rem] 2xl:h-[78.75rem] overflow-hidden grayscale">
              <iframe
   title="location"
   src="https://maps.google.com/maps?q=35.60651,139.74094&z=15&output=embed"
@@ -25,14 +25,14 @@ const HeadQuarter = () => {
 
               {/* <Map /> */}
             </div>
-            <p className="mt-10 md:mt-16 lg:mt-15 2xl:mt-20 md:text-[0.8333rem] lg:text-[0.9500rem] xl92r:text-[1.0417rem] 2xl:text-[1.25rem] leading-[1.875rem] tracking-[0.08em] font-medium font-deca">
+            <p className="mt-2 sm:mt-10 md:mt-16 lg:mt-15 2xl:mt-20 text-[0.75rem] md:text-[0.8333rem] lg:text-[0.9500rem] xl92r:text-[1.0417rem] 2xl:text-[1.25rem] leading-[1.5] sm:leading-[1.875rem] tracking-[0.08em] font-medium font-deca">
               {t('contact.globalheadquarter')}
             </p>
-            <p className="md:text-[0.8333rem] lg:text-[0.9500rem] xl92r:text-[1.0417rem] 2xl:text-[1.25rem] leading-[1.875rem] tracking-[0.08em] font-light font-deca">Japan, </p>
-            <p className="md:text-[0.8333rem] lg:text-[0.9500rem] xl92r:text-[1.0417rem] 2xl:text-[1.25rem] leading-[1.875rem] tracking-[0.08em] font-light font-deca">〒140-0013 Tokyo, </p>
-            <p className="md:text-[0.8333rem] lg:text-[0.9500rem] xl92r:text-[1.0417rem] 2xl:text-[1.25rem] leading-[1.875rem] tracking-[0.08em] font-light font-deca">Shinagawa City, Minamioi,  </p>
-            <p className="md:text-[0.8333rem] lg:text-[0.9500rem] xl92r:text-[1.0417rem] 2xl:text-[1.25rem] leading-[1.875rem] tracking-[0.08em] font-light font-deca">6 Chome−16−4 </p>
-            <p className="md:text-[0.8333rem] lg:text-[0.9500rem] xl92r:text-[1.0417rem] 2xl:text-[1.25rem] leading-[1.875rem] tracking-[0.08em] font-light font-deca">戸浪大森ビル 6F</p>
+            <p className="text-[0.75rem] md:text-[0.8333rem] lg:text-[0.9500rem] xl92r:text-[1.0417rem] 2xl:text-[1.25rem] leading-[1.5] sm:leading-[1.875rem] tracking-[0.08em] font-light font-deca">Japan, </p>
+            <p className="text-[0.75rem] md:text-[0.8333rem] lg:text-[0.9500rem] xl92r:text-[1.0417rem] 2xl:text-[1.25rem] leading-[1.5] sm:leading-[1.875rem] tracking-[0.08em] font-light font-deca">〒140-0013 Tokyo, </p>
+            <p className="text-[0.75rem] md:text-[0.8333rem] lg:text-[0.9500rem] xl92r:text-[1.0417rem] 2xl:text-[1.25rem] leading-[1.5] sm:leading-[1.875rem] tracking-[0.08em] font-light font-deca">Shinagawa City, Minamioi,  </p>
+            <p className="text-[0.75rem] md:text-[0.8333rem] lg:text-[0.9500rem] xl92r:text-[1.0417rem] 2xl:text-[1.25rem] leading-[1.5] sm:leading-[1.875rem] tracking-[0.08em] font-light font-deca">6 Chome−16−4 </p>
+            <p className="text-[0.75rem] md:text-[0.8333rem] lg:text-[0.9500rem] xl92r:text-[1.0417rem] 2xl:text-[1.25rem] leading-[1.5] sm:leading-[1.875rem] tracking-[0.08em] font-light font-deca">戸浪大森ビル 6F</p>
           </div>
         </div>
       </section>
