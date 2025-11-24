30aeae5bd5a273d3f5069c14f325709a9314ffde
mohdsameer-truedax <mohdsameer@truedax.io>
2025-11-24 17:39:09 +0530

    Langcorrection (#34)
    * search mobile responsive
    
    * usecase bug resolve 19/11
    
    * spanish lang correction:
    
    * french lang correction

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { TranslationProvider } from './i18n/TranslationProvider.tsx'
 
 window.addEventListener('unhandledrejection', (event) => {
   if (event.reason?.message?.includes('getInstalledRelatedApps')) {
